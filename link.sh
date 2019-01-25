ef02f812b32f8bbfda231e66d756da3e67da9dac
asyks <aaron@aaronsykes.com>
2019-01-25 14:58:29 -0800

    Fix bad dir var references

--- link.sh
+++ link.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o nounset
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 main () {
   # zsh
